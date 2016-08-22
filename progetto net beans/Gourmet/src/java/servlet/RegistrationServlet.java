8f73be21beb99eb5b0a69ebbd86bb78a3745f2e3
MangustLP <olly.pilat@gmail.com>
2016-08-22 15:02:52 +0200

    Registration Servlet,ritorna errore (Registrazione fallita) se mancan campi

--- progetto net beans/Gourmet/src/java/servlet/RegistrationServlet.java
+++ progetto net beans/Gourmet/src/java/servlet/RegistrationServlet.java
@@ -51,40 +51,38 @@ public class RegistrationServlet extends HttpServlet {
         String email = request.getParameter("Email_");
         String passw = request.getParameter("Password_");
         
-        System.out.println("fname = "+fname);
-        System.out.println("lname = "+lname);
-        System.out.println("uname = "+uname);
-        System.out.println("email = "+email);
-        System.out.println("passw = "+passw);
         
         
-        try{
+        if ((fname != null)&&(lname != null)&&(uname != null)&&(email != null)&&(passw != null)){
         
-            String query = "insert into USERS"
-                            +"(NAME,SURNAME,NICKNAME,EMAIL,PASSWORD,TYPE)"
-                            +"(?,?,?,?,?,?)";    
-            //PreparedStatement ps = (PreparedStatement) manager.getCon().prepareStatement(query)     ;
+            try{
 
-            PreparedStatement ps = (PreparedStatement) manager.getCon().prepareStatement("INSERT INTO USERS (NAME,SURNAME,NICKNAME,EMAIL,PASSWORD,TYPE) VALUES(?,?,?,?,?,?)");
-  
-        
-            ps.setString(1,fname);
-            ps.setString(2,lname);
-            ps.setString(3,uname);
-            ps.setString(4,email);
-            ps.setString(5,passw);
-            ps.setInt(6, 1);
-            
-            ps.executeUpdate();
-            request.setAttribute("messageok", "Registrazione effettuata. Ora puoi fare il login");
-                RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+                PreparedStatement ps = (PreparedStatement) manager.getCon().prepareStatement("INSERT INTO USERS (NAME,SURNAME,NICKNAME,EMAIL,PASSWORD,TYPE) VALUES(?,?,?,?,?,?)");
+
+
+                ps.setString(1,fname);
+                ps.setString(2,lname);
+                ps.setString(3,uname);
+                ps.setString(4,email);
+                ps.setString(5,passw);
+                ps.setInt(6, 1);
+
+                ps.executeUpdate();
+                request.setAttribute("messageOK", "Registrazione effettuata. Ora puoi fare il login");
+                    RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+                    rd.forward(request, response);
+                }
+            catch(Exception se)
+            {
+                request.setAttribute("messageERR", "Registrazione fallita");
+                RequestDispatcher rd = request.getRequestDispatcher("/register.jsp");
                 rd.forward(request, response);
             }
-        catch(Exception se)
-        {
-            request.setAttribute("messageerr", "Registrazione fallita");
-            RequestDispatcher rd = request.getRequestDispatcher("/register.jsp");
-            rd.forward(request, response);
+        }
+        else{
+                request.setAttribute("messageERR", "Registrazione fallita");
+                RequestDispatcher rd = request.getRequestDispatcher("/register.jsp");
+                rd.forward(request, response);
         }
     }
 
