b5c20cfe7ee7dee8aca41b16106cf6a3d49a4627
Denis Gallo <Denis Gallo>
2016-08-25 15:14:36 +0200

    fatto

--- progetto net beans/Gourmet/src/java/db/RistoranteDAO.java
+++ progetto net beans/Gourmet/src/java/db/RistoranteDAO.java
@@ -25,9 +25,10 @@ public class RistoranteDAO {
         
         
         System.out.println(id);
-        String query="SELECT RESTAURANTS.NAME AS NAME, RESTAURANTS.DESCRIPTION AS DESCRIPTION, RESTAURANTS.WEB_SITE_URL AS WEBSITE, COORDINATES.ADDRESS AS ADDRESS, RESTAURANTS.ID_PRICE_RANGE AS PRICE, RESTAURANTS.GLOBAL_VALUE AS VALUE";
-        query+="FROM RESTAURANTS INNER JOIN RESTAURANT_COORDINATE ON RESTAURANTS.ID=RESTAURANT_COORDINATE.ID_RESTAURANT INNER JOIN COORDINATES ON RESTAURANT_COORDINATE.ID_COORDINATE=COORDINATES.ID";
+        String query="SELECT RESTAURANTS.NAME AS NAME, RESTAURANTS.DESCRIPTION AS DESCRIPTION, RESTAURANTS.WEB_SITE_URL AS WEBSITE, COORDINATES.ADDRESS AS ADDRESS, RESTAURANTS.ID_PRICE_RANGE AS PRICE, RESTAURANTS.GLOBAL_VALUE AS VALUE ";
+        query+="FROM RESTAURANTS INNER JOIN RESTAURANT_COORDINATE ON RESTAURANTS.ID=RESTAURANT_COORDINATE.ID_RESTAURANT INNER JOIN COORDINATES ON RESTAURANT_COORDINATE.ID_COORDINATE=COORDINATES.ID ";
         query+="WHERE RESTAURANTS.ID="+id;
+        System.out.println(query);
         ResultSet rs = null;
         try{
             Statement ps = (Statement) connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
@@ -37,11 +38,12 @@ public class RistoranteDAO {
         }   
     
         RistoranteEBJ mioristorante=new RistoranteEBJ();
+        rs.first();
         mioristorante.setId(id);
         mioristorante.setName(rs.getString("NAME"));
         mioristorante.setDescription(rs.getString("DESCRIPTION"));
-        getPhotos(mioristorante);
-        getCuisines(mioristorante);
+        getPhotos(mioristorante, connection);
+        getCuisines(mioristorante, connection);
         mioristorante.setWebsite(rs.getString("WEBSITE"));
         mioristorante.setAddress(rs.getString("ADDRESS"));
         mioristorante.setPrice(Integer.parseInt(rs.getString("PRICE")));
@@ -49,20 +51,21 @@ public class RistoranteDAO {
         return mioristorante;
     }
     
-    private void getPhotos(RistoranteEBJ mioristorante) throws SQLException{
+    private void getPhotos(RistoranteEBJ mioristorante, Connection connection) throws SQLException{
         String query="SELECT PATH FROM PHOTOS INNER JOIN RESTAURANTS ON PHOTOS.ID_RESTAURANT=RESTAURANTS.ID WHERE RESTAURANTS.ID="+mioristorante.getId();
-        Statement ps = (Statement) manager.getCon().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+        Statement ps = (Statement) connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
         ResultSet rs = ps.executeQuery(query);
         ArrayList<String> mioarray=new ArrayList();
+        rs.first();
         while(!rs.isAfterLast()){
             mioarray.add(rs.getString("PATH"));
             rs.next();
         }
         mioristorante.setPath((String[]) mioarray.toArray());     
     }
-    private void getCuisines(RistoranteEBJ mioristorante) throws SQLException{
+    private void getCuisines(RistoranteEBJ mioristorante, Connection connection) throws SQLException{
         String query="SELECT NAME FROM CUISINES INNER JOIN RESTAURANT_CUISINE ON RESTAURANT_CUISINE.ID_CUISINE=CUISINES.ID WHERE RESTAURANT_CUISINE.ID_RESTAURANT="+mioristorante.getId();
-        Statement ps = (Statement) manager.getCon().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+        Statement ps = (Statement) connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
         ResultSet rs = ps.executeQuery(query);
         ArrayList<String> mioarray=new ArrayList();
         while(!rs.isAfterLast()){
